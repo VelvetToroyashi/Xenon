2e6538fa15f551bbb8fa9ed753e89098a9261ada
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2024-06-15 02:42:19 -0400

    feat: Add timespan parser

---
+++ Xenon/Services/MicroTimeSpanParser.cs
@@ -0,0 +1,37 @@
+using System.Text.RegularExpressions;
+using Remora.Commands.Parsers;
+using Remora.Commands.Results;
+using Remora.Results;
+
+namespace Xenon.Services;
+
+public partial class MicroTimeSpanParser : AbstractTypeParser<TimeSpan>
+{
+    [GeneratedRegex("(?<Quantity>[0-9]{1,4}(?<Value>y|mo|w|d|h|m)){1,3}")]
+    private partial Regex TimeMatch();
+
+    public override ValueTask<Result<TimeSpan>> TryParseAsync(string token, CancellationToken ct = default)
+    {
+        var match = TimeMatch().Match(token);
+
+        if (!match.Success)
+        {
+            return ValueTask.FromResult(Result<TimeSpan>.FromError(new ParsingError<TimeSpan>("Invalid time format.")));
+        }
+
+        var quantity = int.Parse(match.Groups["Quantity"].Value[..^1]);
+        var value = match.Groups["Value"].Value[^1] switch
+        {
+            'y' => TimeSpan.FromDays(365 * quantity),
+            'o' => TimeSpan.FromDays(30  * quantity),
+            'w' => TimeSpan.FromDays(7   * quantity),
+            'd' => TimeSpan.FromDays(quantity),
+            'h' => TimeSpan.FromHours(quantity),
+            'm' => TimeSpan.FromMinutes(quantity),
+            's' => TimeSpan.FromSeconds(quantity),
+            _   => throw new ArgumentOutOfRangeException()
+        };
+
+        return ValueTask.FromResult<Result<TimeSpan>>(value);
+    }
+}
